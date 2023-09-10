0a57d176f5406f6f96089317d4fab1d582f71e7d
Alain Forte <alain.forte@c.teamexos.com>
2023-09-10 14:58:19 +0200

    Icons

--- app/src/Layout/Tabs.tsx
+++ app/src/Layout/Tabs.tsx
@@ -5,14 +5,37 @@ import {ChatScreen} from '../screens/ChatScreen';
 import {ProfileScreen} from '../screens/ProfileScreen';
 import {MatchesScreen} from '../screens/MatchesScreen';
 
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faComment} from '@fortawesome/free-solid-svg-icons/faComment';
+import {faUser} from '@fortawesome/free-solid-svg-icons/faUser';
+import {faHeart} from '@fortawesome/free-solid-svg-icons/faHeart';
+
 const Tab = createBottomTabNavigator();
 
 const TabsLayout: React.FC = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Matches" component={MatchesScreen} />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => <FontAwesomeIcon icon={faComment} />,
+        }}
+        name="Chat"
+        component={ChatScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => <FontAwesomeIcon icon={faUser} />,
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => <FontAwesomeIcon icon={faHeart} />,
+        }}
+        name="Matches"
+        component={MatchesScreen}
+      />
     </Tab.Navigator>
   );
 };
