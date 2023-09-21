692393f9e1bfc09b4bade05209b00e388171f518
Alain Forte <alain.forte@c.teamexos.com>
2023-09-21 09:57:46 +0200

    ChatScreen

--- app/src/screens/ChatScreen.tsx
+++ app/src/screens/ChatScreen.tsx
@@ -2,16 +2,23 @@ import styled from '@emotion/native';
 import { useTheme } from '@emotion/react';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
 import { MessageBubble } from '../components/MessageBubble';
 import { API } from 'aws-amplify';
-import { messagesByProfileId } from '../graphql/queries';
 import { useAuthenticator } from '@aws-amplify/ui-react-native';
 import { userSelector } from '../utils/aws';
-import { Message, MessagesByProfileIdQuery, SenderType } from '../types/graphql';
+import {
+  Message,
+  MessagesByProfileIdAndCreatedAtQuery,
+  ModelSortDirection,
+  SendChatMessageMutation,
+  SenderType,
+} from '../types/graphql';
 import { GraphQLQuery } from '@aws-amplify/api';
-
+import { sendChatMessage } from '../graphql/mutations';
+import { messagesByProfileIdAndCreatedAt } from '../graphql/queries';
+import { Button } from '../components/Button';
 
 const ChatContainer = styled.View(({ theme }) => ({
   flex: 1,
@@ -43,21 +50,40 @@ const ChatInput = styled.TextInput(({ theme }) => ({
 export const ChatScreen: React.FC = () => {
   const { user } = useAuthenticator(userSelector);
   const theme = useTheme();
+  const messageLogRef = useRef<ScrollView>(null);
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [chatlog, setChatlog] = useState<Array<Message | null>>([]);
+  const [aiTyping, setAITyping] = useState(false);
+  const [traits, setTraits] = useState({});
 
   const fetchMessages = async (id: string) => {
     setLoading(true);
     try {
-      const result = await API.graphql<GraphQLQuery<MessagesByProfileIdQuery>>({
-        query: messagesByProfileId,
+      const result = await API.graphql<
+        GraphQLQuery<MessagesByProfileIdAndCreatedAtQuery>
+      >({
+        query: `query GetMessages($profileId: ID!) {
+          messagesByProfileIdAndCreatedAt(profileId: $profileId, sortDirection: DESC, limit: 10) {
+            items {
+              id
+              content
+              metadata
+              sender
+              createdAt
+            }
+          }
+        }`,
         variables: {
-          profileId: id,
+          profileId: id
         },
       });
-
-      const messages = result?.data?.messagesByProfileId?.items;
+      const traits =
+        result?.data?.messagesByProfileIdAndCreatedAt?.items?.[0]?.profile
+          ?.traits ?? '{}';
+      const messages =
+        result?.data?.messagesByProfileIdAndCreatedAt?.items ??
+        ([] as Message[]);
 
       if (!messages) {
         console.log(
@@ -68,9 +94,10 @@ export const ChatScreen: React.FC = () => {
         return;
       }
 
-      console.log({ id }, JSON.stringify(messages, null, 4));
-
-      setChatlog(messages);
+      setTraits(JSON.parse(traits));
+      console.log('MESSAGES', JSON.stringify(messages, null, 4))
+      setChatlog(messages.reverse());
+      scrollToEnd();
     } catch (e) {
       console.log('error fetching messages', { id }, e);
     } finally {
@@ -82,7 +109,71 @@ export const ChatScreen: React.FC = () => {
     if (user?.attributes?.sub) {
       fetchMessages(user.attributes.sub);
     }
-  },[]);
+  }, []);
+
+  const sendMessageMutation = async (message: string) => {
+    setAITyping(true);
+    try {
+      const response = await API.graphql<GraphQLQuery<SendChatMessageMutation>>(
+        {
+          query: sendChatMessage,
+          variables: {
+            userId: user?.attributes?.sub,
+            message: message,
+            currentProfile: JSON.stringify(traits),
+            history: JSON.stringify(chatlog.slice(-10)),
+          },
+        },
+      );
+
+      const reponseJSON = JSON.parse(
+        response.data?.sendChatMessage ?? '{}',
+      );
+
+      console.log(
+        'RESPONSE',
+        JSON.stringify(
+          reponseJSON,
+          null,
+          4,
+        ),
+      );
+
+      return reponseJSON as {
+        nextMessage: string;
+        profile: Record<string, any>;
+      };
+    } catch (e) {
+      console.log('error sending message', e);
+    } finally {
+      setAITyping(false);
+    }
+  };
+
+  const scrollToEnd = () => messageLogRef.current?.scrollToEnd({ animated: false });
+  const appendMessageToChatlog = ({ content, sender}: Message) => {
+    setChatlog(chatlog => [
+      ...chatlog,
+      { content, sender } as Message,
+    ]);
+    
+    scrollToEnd();
+  }
+
+  const onPressSend = async () => {
+    setMessage('');
+
+    appendMessageToChatlog({ content: message, sender: SenderType.USER } as Message);
+    const res = await sendMessageMutation(message);
+    appendMessageToChatlog({ content: res?.nextMessage, sender: SenderType.AI } as Message);
+    
+    if (res?.profile) {
+      setTraits({
+        ...traits,
+        ...res?.profile,
+      });
+    }
+  }
 
   if (loading) {
     return (
@@ -94,20 +185,29 @@ export const ChatScreen: React.FC = () => {
 
   return (
     <ChatContainer>
-      <MessageLog showsVerticalScrollIndicator={false}>
-        {chatlog.map((chat, index) => chat && (
-          <MessageBubble key={index} message={chat?.content} sent={chat.sender === SenderType.USER} />
-        ))}
+      <MessageLog showsVerticalScrollIndicator={false} ref={messageLogRef}>
+        {chatlog.length <= 9 && <MessageBubble message={'Hello 👋! This is the beginning of your conversation. Start by introducing yourself and sharing what you like.'} sent={false} />}
+
+        {chatlog.map(
+          (chat, index) =>
+            chat && (
+              <MessageBubble
+                key={index}
+                message={chat?.content}
+                sent={chat.sender === SenderType.USER}
+              />
+            ),
+        )}
+
+        {aiTyping && (
+          <MessageBubble key={chatlog.length} message={'...'} sent={false} />
+        )}
       </MessageLog>
 
       <InputArea>
         <ChatInput value={message} onChangeText={setMessage} />
         <TouchableOpacity
-          onPress={() => {
-            // TODO: Handle sending the message here
-            setChatlog(chatlog => [...chatlog, { content: message, sender: SenderType.USER } as Message]);
-            setMessage(''); // Reset the input field
-          }}>
+          onPress={onPressSend}>
           <FontAwesomeIcon
             icon={faPaperPlane}
             size={20}
